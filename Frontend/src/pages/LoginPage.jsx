b1f57b6db6ceae30d4c784a6e987ca8d45b60fd5
AnkitaArvindDagade <ankita.dagade2005@gmail.com>
2025-08-25 15:03:45 +0530

    Updated LandingPage and LoginPage

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -84,7 +84,7 @@ const LoginPage = () => {
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md bg-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 >
                   <option value="student">Student</option>
-                  <option value="mentor">Mentor</option>
+                  <option value="mentor">clerk</option>
                   <option value="admin">Admin</option>
                 </select>
               </div>
